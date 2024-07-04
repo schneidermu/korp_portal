4c7fe24a3b26ccd494e7a6ffda2af6a3028da4e8
rosvodres <agrngabena@gmail.com>
2024-07-04 08:27:29 +0000

    add profile

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use('/front/authentication', express.static(path.join(__dirname, 'front/auth
 app.use('/front/styles', express.static(path.join(__dirname, 'front/styles')));
 app.use('/front/src', express.static(path.join(__dirname, 'front/src')));
 app.use('/front/desktop', express.static(path.join(__dirname, 'front/desktop')));
+app.use('/front/profile', express.static(path.join(__dirname, 'front/profile')));
+app.use('/front/profile', express.static(path.join(__dirname, 'front/profile')));
+
+app.get('/orgstruct', (req, res) => {
+  res.sendFile(path.join(__dirname, 'front/orgstruct/orgstruct.html'));
+});
+
+app.get('/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'front/profile/profile.html'));
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'front/auth/auth.html'));
