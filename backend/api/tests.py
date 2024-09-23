d5cb4ae7180f69a8759c8c24fb327d24cba19113
alexeybur <schneider.mark14@gmail.com>
2024-09-23 12:16:10 +0300

    change endpoint names

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -13,26 +13,26 @@ class TestApi(TestCase):
         Token.objects.create(user=self.user)
 
     def test_polls_api(self):
-        request = self.factory.get('/polls/')
+        request = self.factory.get('polls')
         response = self.client.get(request)
         self.assertEqual(response.status_code, 200)
 
     def test_news_api(self):
-        request = self.factory.get('/news/')
+        request = self.factory.get('news')
         response = self.client.get(request)
         self.assertEqual(response.status_code, 200)
 
     def test_colleagues_api(self):
-        request = self.factory.get('/colleagues/')
+        request = self.factory.get('colleagues')
         response = self.client.get(request)
         self.assertEqual(response.status_code, 200)
 
     def test_org_structure_api(self):
-        request = self.factory.get('/org-structure/')
+        request = self.factory.get('org-structure')
         response = self.client.get(request)
         self.assertEqual(response.status_code, 200)
 
     def test_organization_api(self):
-        request = self.factory.get('/organization/')
+        request = self.factory.get('organization')
         response = self.client.get(request)
         self.assertEqual(response.status_code, 200)
