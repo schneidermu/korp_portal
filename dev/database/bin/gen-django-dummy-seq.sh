dcb79694bb9bbc34ae557efadb2868d31231bfc7
Matthew Dreamer <six283185@yandex.ru>
2024-12-04 18:08:01 +0300

    Exclude UUID id from dummy data id sequence update

--- dev/database/bin/gen-django-dummy-seq.sh
+++ dev/database/bin/gen-django-dummy-seq.sh
@@ -2,5 +2,7 @@
 set -eu
 
 awk '/COPY [_.a-z0-9]+ \(.*id\s*,/ { print $2 }' database/sql/django-dummy.sql | while read -r table; do
+  # Ignore UUID id fields.
+  [ "$table" = "public.employees_employee" ] && continue
   echo "SELECT pg_catalog.setval('${table}_id_seq', (SELECT MAX(id) FROM $table), true);"
 done
