c39790def6c8c3a51b9790fb12af56e514693cca
Matthew Dreamer <six283185@yandex.ru>
2024-12-04 17:32:58 +0300

    Fix dummy data id sequence generation
    The regexp assumed the id is present *and* is mentioned as the first
    column, which is not always the case.

--- dev/database/bin/gen-django-dummy-seq.sh
+++ dev/database/bin/gen-django-dummy-seq.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-awk '/COPY [_.a-z0-9]+ \(id,/ { print $2 }' database/sql/django-dummy.sql | while read -r table; do
+awk '/COPY [_.a-z0-9]+ \(.*id\s*,/ { print $2 }' database/sql/django-dummy.sql | while read -r table; do
   echo "SELECT pg_catalog.setval('${table}_id_seq', (SELECT MAX(id) FROM $table), true);"
 done
