260225a78d56abcfd2ab4aec36ff52ca269bb7c7
Matthew Dreamer <six283185@yandex.ru>
2025-01-10 20:43:48 +0300

    Prevent scroll when modal window is open

--- react/src/common/Overlay.tsx
+++ react/src/common/Overlay.tsx
@@ -1,9 +1,26 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 import clsx from "clsx/lite";
 import { motion } from "motion/react";
 
 export const Overlay = ({ children }: { children: ReactNode }) => {
+  useEffect(() => {
+    const html = document.documentElement;
+
+    const w1 = html.offsetWidth;
+    html.style.overflow = "hidden";
+    const w2 = html.offsetWidth;
+
+    const scrollBarWidth = w2 - w1;
+
+    html.style.paddingRight = `${scrollBarWidth}px`;
+
+    return () => {
+      html.style.overflow = "unset";
+      html.style.paddingRight = "0";
+    };
+  }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
