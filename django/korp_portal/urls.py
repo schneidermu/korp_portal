76c21ea919a290996d4a5cf647e7e9af537867b3
Mark Schneider <schneider.mark14@gmail.com>
2025-02-12 15:01:34 +0300

    [upd] Django to host media and static instead of Nginx

--- django/korp_portal/urls.py
+++ django/korp_portal/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 
@@ -38,3 +40,6 @@ urlpatterns += [
         name='schema-redoc'
     ),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
