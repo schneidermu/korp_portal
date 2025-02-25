5791b52f576ae2669b36c7cc4e9713a7c9c104fc
Matthew Dreamer <six283185@yandex.ru>
2025-02-25 15:50:05 +0300

    dummy: improve dummy data

--- kp-dev/dummy/dummy/util.py
+++ kp-dev/dummy/dummy/util.py
@@ -26,3 +26,11 @@ RU_TO_LATIN = load_json("data/ru_to_latin.json")
 
 def ru_to_latin(s: str) -> str:
     return "".join(RU_TO_LATIN.get(c, c) for c in s)
+
+
+def pronounce_years_ru(y: int) -> str:
+    if y % 10 == 1 and y != 11:
+        return f"{y} год"
+    if y % 10 in (2, 3, 4) and y // 10 != 1:
+        return f"{y} года"
+    return f"{y} лет"
