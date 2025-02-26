0984f28a6408d2094008e0508cf494c3fe3020f0
Matthew Dreamer <six283185@yandex.ru>
2025-02-26 22:53:57 +0300

    feat(dummy): wrap loading in a single transaction

--- kp-dev/dummy/load.sh
+++ kp-dev/dummy/load.sh
@@ -6,4 +6,4 @@ set -eu
 cd "$(dirname "$0")"
 
 python dummy.py |
-  docker compose exec -T database psql -U "${POSTGRES_USER_DJANGO}" -d "${POSTGRES_DB_DJANGO}"
+  docker compose exec -T database psql -1 -U "${POSTGRES_USER_DJANGO}" -d "${POSTGRES_DB_DJANGO}"
