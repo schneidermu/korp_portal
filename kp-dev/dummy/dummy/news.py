a775c62545057a55c65b65181fb6eff7821637ed
Matthew Dreamer <six283185@yandex.ru>
2025-02-27 01:10:58 +0300

    feat(dummy): use random dates for news

--- kp-dev/dummy/dummy/news.py
+++ kp-dev/dummy/dummy/news.py
@@ -1,24 +1,37 @@
+import datetime
 from itertools import permutations
+
+from .util import random_date_between
 
 
 def gen():
     title = "Ленское БВУ участвовало в забеге «Беги Форест, беги»"
     text = "Ленское БВУ приняло участие в забеге «Беги, Форест, беги». Сотрудники бассейного водного управления присоединились к мероприятию, чтобы поддержать здоровый образ жизни и привлечь внимание к проблемам экологии. Забег проходил в одном из парков Москвы, участники соревновались на дистанциях от 5 до 10 километров."
 
+    today = datetime.date.today()
+
     print(
         "COPY public.homepage_news (id, is_published, created_at, pub_date, title, text, video) FROM stdin;"
     )
+    prev_date = datetime.date(2024, 1, 1)
+    i = 0
     for i in range(24):
+        date = random_date_between(prev_date, today)
+        prev_date = date
         print(
             i + 1,
             "t",
-            f"2024-05-{i+1:02d} 10:00:00",
-            f"2024-05-{i+1:02d} 12:00:00",
+            f"{date.isoformat()} 10:00:00",
+            f"{date.isoformat()} 12:00:00",
             f"({i+1}) {title}",
             text,
             r"\N",
             sep="\t",
         )
+        if date == today:
+            break
+    num_news = i + 1
+
     print(r"\.")
     print()
     print("SELECT pg_catalog.setval('public.homepage_news_id_seq', 24, true);")
@@ -27,8 +40,8 @@ def gen():
     print(
         "COPY public.homepage_news_organization (id, organization_id, news_id) FROM stdin;"
     )
-    for i in range(24):
-        print(i + 1, 1, i + 1, sep="\t")
+    for i in range(num_news):
+        print(i + 1, 2, i + 1, sep="\t")
     print(r"\.")
     print()
     print(
@@ -38,6 +51,8 @@ def gen():
 
     print("COPY public.homepage_attachment (publication_id, id, image) FROM stdin;")
     for i, imgs in enumerate(permutations(range(4), r=4)):
+        if i >= num_news:
+            break
         for j, img in enumerate(imgs):
             print(i + 1, 4 * i + j + 1, f"news_images/{img+1:02d}.png", sep="\t")
     print(r"\.")
