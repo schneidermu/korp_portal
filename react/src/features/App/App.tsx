05de9059ad36e589c9a320ca8c47494a92988ad6
Matthew Dreamer <six283185@yandex.ru>
2025-03-03 15:55:43 +0300

    chore(react): install Chakra UI

--- react/src/features/App/App.tsx
+++ react/src/features/App/App.tsx
@@ -1,5 +1,9 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { system } from "./theme";
+
 import { Feed } from "@/features/Feed/Feed";
 import { NotFound } from "@/features/NotFound/NotFound";
 import { UserProfile } from "@/features/Profile/Profile";
@@ -10,18 +14,20 @@ import { Page } from "./parts/Page";
 
 export const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route element={<Page />}>
-          <Route path="/" element={<UserProfile />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/profile/:userId" element={<UserProfile />} />
-          <Route path="list/:query?" element={<UserList />} />
-          <Route path="/units/:query?" element={<UnitList />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <ChakraProvider value={system}>
+      <HashRouter>
+        <Routes>
+          <Route element={<Page />}>
+            <Route path="/" element={<UserProfile />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile/:userId" element={<UserProfile />} />
+            <Route path="list/:query?" element={<UserList />} />
+            <Route path="/units/:query?" element={<UnitList />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ChakraProvider>
   );
 };
