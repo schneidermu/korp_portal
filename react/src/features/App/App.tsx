f5a53358f6886d1e33c1c05d482bb760563553bb
Matthew Dreamer <six283185@yandex.ru>
2025-03-03 15:55:43 +0300

    chore(react): install Chakra UI

--- react/src/features/App/App.tsx
+++ react/src/features/App/App.tsx
@@ -1,5 +1,7 @@
 import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import { ChakraProvider, createSystem, defineConfig } from "@chakra-ui/react";
+
 import { Feed } from "@/features/Feed/Feed";
 import { NotFound } from "@/features/NotFound/NotFound";
 import { UserProfile } from "@/features/Profile/Profile";
@@ -8,20 +10,33 @@ import { UserList } from "@/features/UserList/UserList";
 
 import { Page } from "./parts/Page";
 
+const config = defineConfig({
+  strictTokens: true,
+  theme: {
+    tokens: {
+      colors: {},
+    },
+  },
+});
+
+const system = createSystem(config);
+
 export const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route element={<Page />}>
-          <Route path="/" element={<UserProfile />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/profile/:userId" element={<UserProfile />} />
-          <Route path="list/:query?" element={<UserList />} />
-          <Route path="/units/:query?" element={<UnitList />} />
-          <Route path="/404" element={<NotFound />} />
-          <Route path="*" element={<Navigate to="/404" replace />} />
-        </Route>
-      </Routes>
-    </HashRouter>
+    <ChakraProvider value={system}>
+      <HashRouter>
+        <Routes>
+          <Route element={<Page />}>
+            <Route path="/" element={<UserProfile />} />
+            <Route path="/feed" element={<Feed />} />
+            <Route path="/profile/:userId" element={<UserProfile />} />
+            <Route path="list/:query?" element={<UserList />} />
+            <Route path="/units/:query?" element={<UnitList />} />
+            <Route path="/404" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ChakraProvider>
   );
 };
--- react/vite.config.ts
+++ react/vite.config.ts
@@ -2,11 +2,12 @@ import path from "node:path";
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
   const conf = {
     base: "/vite/",
-    plugins: [react()],
+    plugins: [react(), tsconfigPaths()],
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
