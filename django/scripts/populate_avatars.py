6f93e082f1a1978c6e9dbbea9b0b5a80c5886911
Matthew Dreamer <six283185@yandex.ru>
2025-03-19 11:28:49 +0300

    refactor(django): refactor and fix populate_avatars script

--- django/scripts/populate_avatars.py
+++ django/scripts/populate_avatars.py
@@ -1,6 +1,6 @@
 import base64
 import logging
-import uuid
+import warnings
 
 import requests
 from django.core.files.base import ContentFile
@@ -9,58 +9,60 @@ from korp_portal.backends import cursor
 
 logger = logging.getLogger(__name__)
 
+EXT = "png"
 
-def base64_to_uploaded_file(base64_string, filename=None):
-    format, imgstr = (
-        base64_string.split(";base64,")
-        if ";base64," in base64_string
-        else ("", base64_string)
-    )
-    ext = format.split("/")[-1] if "/" in format else "bin"
 
-    if not filename:
-        filename = f"{uuid.uuid4()}.{ext}"
+def liferay_fetch_avatar(url: str, user_id: int, p_auth: str, jsessionid: str):
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        res = requests.post(
+            url,
+            cookies={"JSESSIONID": jsessionid},
+            data={"p_auth": p_auth, "userId": user_id},
+            verify=False,
+        )
+    data = res.json()
+    status = data["status"]
+    if status != 1:
+        return None
+    return data["result"]["id"]
 
-    decoded_file = base64.b64decode(imgstr)
 
-    content_file = ContentFile(decoded_file, name=filename)
+def upload_base64_avatar(email: str, avatar_base64: str):
+    email_address = email.split("@")[0]
+    filename = f"imported-avatars/{email_address}.{EXT}"
+
+    avatar_binary = base64.b64decode(avatar_base64)
+
+    content_file = ContentFile(avatar_binary, name=filename)
     result = UploadedFile.objects.create(file=content_file)
 
     return result.file
 
 
 def run(*args):
-    url, p_auth, JSESSIONID = args
+    url, p_auth, jsessionid = args
 
-    cookies = {"JSESSIONID": JSESSIONID}
+    update_cnt = 0
 
-    cursor.execute("SELECT emailaddress, userid FROM user_")
-
-    list_of_users = cursor.fetchall()
-
-    for email, userid in list_of_users:
+    cursor.execute("SELECT emailaddress, userid FROM user_ ORDER BY userid")
+    for email, user_id in cursor.fetchall():
         try:
             user = Employee.objects.get(email=email)
         except Employee.DoesNotExist:
-            logger.info(f"User with email {email} not found.")
+            print(f"User with email {email} not found.")
             continue
 
-        data = {
-            "p_auth": p_auth,
-            "userId": userid,
-        }
-
-        response = requests.get(url, cookies=cookies, data=data)
-
-        base64_string = response.json().get["id"]
-
-        if base64_string is None:
-            logger.info(f"User with email {email} does not have an avatar.")
+        avatar = liferay_fetch_avatar(url, user_id, p_auth, jsessionid)
+        if avatar is None:
             continue
 
-        file = base64_to_uploaded_file(base64_string)
+        file = upload_base64_avatar(email, avatar)
 
         user.avatar = f"/media/{file}"
         user.save()
 
-        logger.info(f"User with email {email} has been updated with avatar: {file}.")
+        print(f"User with email {email} has been updated with avatar: {file}.")
+        update_cnt += 1
+
+    print(f"A total of {update_cnt} user avatars were imported")
