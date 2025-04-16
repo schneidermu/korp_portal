08a2b39a018c21d62eb6b065bcd687f58abcb4f9
Mark Schneider <schneider.mark14@gmail.com>
2025-04-16 09:17:07 +0300

    [add] Year of training to Model + migrations

--- django/employees/models.py
+++ django/employees/models.py
@@ -415,6 +415,10 @@ class University(AbstractWithPhotoNameModel):
 class Training(AbstractWithPhotoNameModel):
     """Модель повышения квалификации."""
 
+    year = models.IntegerField(
+        verbose_name="Год повышения квалификации", blank=True, null=True
+    )
+
     class Meta:
         verbose_name = "запись повышения квалификации"
         verbose_name_plural = "записи повышений квалификации"
