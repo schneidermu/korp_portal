1a8e4350e2fdbc8c939ac13e12e907174ac4f70e
Matthew Dreamer <six283185@yandex.ru>
2025-04-29 12:12:45 +0300

    feat(nextcloud): embed Nextcloud into pages

--- react/src/features/Nextcloud/Nextcloud.tsx
+++ react/src/features/Nextcloud/Nextcloud.tsx
@@ -1,18 +1,63 @@
-import { NewPage } from "@/features/App/comps/NewPage.tsx";
-import { useAuth } from "@/features/auth/slice";
 import { useEffect } from "react";
 
-export const NextcloudPage = () => {
+import { Box } from "@chakra-ui/react";
+
+import { NEXTCLOUD_PREFIX } from "@/app/const";
+
+import { useAuth } from "@/features/auth/slice";
+
+import { NewPage } from "@/features/App/comps/NewPage.tsx";
+import { AnimatePage } from "@/features/App/comps/PageSkel";
+
+const useSetNextcloudCookie = () => {
   const { email, token } = useAuth();
 
   useEffect(() => {
     const authorization = btoa(`${email}:${token}`);
-    document.cookie = `nextcloud_authorization=${authorization}; Secure; Path=/`;
+    document.cookie = `nextcloud_authorization=${authorization}; Path=/`;
   }, [email, token]);
+};
+
+export const NextcloudPage = () => {
+  useSetNextcloudCookie();
 
   return (
     <NewPage>
-      <iframe width="100%" height="100%" src="/api/kp/nextcloud/" />
+      <Box
+        borderWidth={1}
+        borderColor="gray.1"
+        borderRadius="1"
+        overflow="hidden"
+      >
+        <iframe width="100%" height="100%" src={NEXTCLOUD_PREFIX + "/"} />
+      </Box>
     </NewPage>
   );
+};
+
+export const Nextcloud = () => {
+  useSetNextcloudCookie();
+
+  return (
+    <AnimatePage>
+      <Box
+        borderWidth={1}
+        borderColor="gray.1"
+        borderRadius="1"
+        overflow="hidden"
+      >
+        <iframe width="100%" height="1200px" src={NEXTCLOUD_PREFIX + "/"} />
+      </Box>
+    </AnimatePage>
+  );
+};
+
+export const NextcloudStandalone = () => {
+  useSetNextcloudCookie();
+
+  useEffect(() => {
+    window.location.replace(NEXTCLOUD_PREFIX + "/");
+  }, []);
+
+  return <div></div>;
 };
