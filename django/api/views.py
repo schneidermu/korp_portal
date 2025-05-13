0281d43c66e2937b8d31d065215d3d3e2232ccf1
Mark Schneider <schneider.mark14@gmail.com>
2025-05-13 16:39:34 +0300

    [upd] Add Cookies for nextcloud auth

--- django/api/views.py
+++ django/api/views.py
@@ -2,7 +2,7 @@ import base64
 from datetime import datetime
 
 from django_filters.rest_framework import DjangoFilterBackend
-from djoser.views import UserViewSet
+from djoser.views import TokenCreateView, UserViewSet
 from rest_framework import filters, generics, status, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import action
@@ -387,3 +387,53 @@ class CompetenceListView(generics.ListAPIView):
         ).order_by("name")
 
         return queryset.order_by("name")
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CustomTokenCreateView(TokenCreateView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == status.HTTP_200_OK:
+            try:
+                auth_token_key = response.data.get("auth_token")
+
+                if not auth_token_key:
+                    return response
+
+                try:
+                    token_obj = Token.objects.select_related("user").get(
+                        key=auth_token_key
+                    )
+                except Token.DoesNotExist:
+                    return response
+
+                user_email = token_obj.user.email
+
+                if not user_email:
+                    return response
+
+                credentials_to_encode = f"{user_email}:{auth_token_key}"
+                credentials_as_bytes = credentials_to_encode.encode("utf-8")
+
+                encoded_credentials_bytes = base64.b64encode(credentials_as_bytes)
+
+                encoded_credentials_str = encoded_credentials_bytes.decode("utf-8")
+
+                response.set_cookie(
+                    key="nextcloud_authorization",
+                    value=encoded_credentials_str,
+                    secure=False,
+                    httponly=True,
+                    path="/",
+                    samesite="Lax",
+                )
+
+            except Exception as e:
+                pass
+
+        return response
