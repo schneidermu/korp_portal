0df1aca93fdb48f431dd4722289fd7fb5c9113c9
Matthew Dreamer <six283185@yandex.ru>
2025-05-14 18:27:53 +0300

    feat(navbar): update new navbar
    - Add Nextcloud links.
    - Comment out old UI link.

--- react/src/features/App/parts/NewNavBar.tsx
+++ react/src/features/App/parts/NewNavBar.tsx
@@ -1,31 +1,46 @@
 import { Stack, Text } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
+import { NEXTCLOUD_PREFIX } from "@/app/const.ts";
+
 import { useAuth } from "@/features/auth/slice";
 
 export const NewNavBar = () => {
   const { orgId, userId } = useAuth();
 
-  const links = [
-    ["Рабочий стол", "/feed"],
-    ["Мой профиль", userId ? `/new/profile/${userId}` : "/"],
-    [
-      "Список сотрудников",
-      orgId !== null ? `/new/list?orgId=${orgId}` : "/list",
-    ],
-    ["Список отделов", orgId !== null ? `/units?org=${orgId}` : "/units"],
-  ] as const;
+  const links: { name: string; link: string; anchor?: boolean }[] = [
+    { name: "Новости", link: "/new/feed" },
+    { name: "Мой профиль", link: userId ? `/new/profile/${userId}` : "/" },
+    {
+      name: "Список сотрудников",
+      link: orgId !== null ? `/new/list?orgId=${orgId}` : "/list",
+    },
+    // {
+    //   name: "Список отделов",
+    //   link: orgId !== null ? `/units?org=${orgId}` : "/units",
+    // },
+    { name: "Облако", link: "/new/nextcloud" },
+    {
+      name: "Облако (полный экран)",
+      link: NEXTCLOUD_PREFIX + "/",
+      anchor: true,
+    },
+  ];
 
   return (
     <Stack fontSize="2xl" color="blue.2" gap="9">
-      {links.map(([text, url]) => (
+      {links.map((link) => (
         <Text
-          key={text}
+          key={link.link}
           asChild
           fontWeight="semibold"
           textDecoration={{ _hover: "underline" }}
         >
-          <Link to={url}>{text}</Link>
+          {link.anchor ? (
+            <a href={link.link}>{link.name}</a>
+          ) : (
+            <Link to={link.link}>{link.name}</Link>
+          )}
         </Text>
       ))}
     </Stack>
