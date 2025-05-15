5205b4fec961f1b62fe89f6575c47aac6f3c1c84
Matthew Dreamer <six283185@yandex.ru>
2025-05-15 16:56:54 +0300

    feat(nextcloud): embed in the new UI

--- react/src/features/Nextcloud/Nextcloud.tsx
+++ react/src/features/Nextcloud/Nextcloud.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Separator, Stack } from "@chakra-ui/react";
 
 import { NEXTCLOUD_PREFIX } from "@/app/const";
 
@@ -7,14 +7,21 @@ import { NewPage } from "@/features/App/comps/NewPage.tsx";
 export const NextcloudPage = () => {
   return (
     <NewPage>
-      <Box
-        borderWidth={1}
-        borderColor="gray.1"
-        borderRadius="1"
-        overflow="hidden"
-      >
-        <iframe width="100%" height="100%" src={NEXTCLOUD_PREFIX + "/"} />
-      </Box>
+      <Stack gap="6" h="full">
+        <Heading as="h1" fontSize="3xl" color="blue.4">
+          Облачное хранилище
+        </Heading>
+        <Separator borderWidth={1} borderColor="gray.4" />
+        <Box
+          borderWidth={1}
+          borderColor="gray.1"
+          borderRadius="1"
+          overflow="hidden"
+          h="full"
+        >
+          <iframe width="100%" height="100%" src={NEXTCLOUD_PREFIX + "/"} />
+        </Box>
+      </Stack>
     </NewPage>
   );
 };
