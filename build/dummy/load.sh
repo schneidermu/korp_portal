9202abc542d0045ebe78dfa14dc550c42c86ba91
Matthew Dreamer <six283185@yandex.ru>
2025-07-01 10:56:22 +0300

    feat(dummy): run python script in a container

--- build/dummy/load.sh
+++ build/dummy/load.sh
@@ -5,5 +5,5 @@ set -eu
 
 cd "$(dirname "$0")"
 
-python -B dummy.py |
+docker run -v "$(realpath ../dummy):/app" -v ./.env:/.env python:3.12 python /app/dummy.py |
   docker compose -p "$COMPOSE_NAME" exec -T db psql -1 -U "$DB_USER_DJANGO" -d "$DB_NAME_DJANGO"
