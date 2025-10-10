6c511e58eeeb19a274341846a2a37739882de61b
Matthew Dreamer <six283185@yandex.ru>
2025-10-10 13:56:02 +0300

    refactor: remove unused components

--- react/src/features/App/comps/Page.tsx
+++ react/src/features/App/comps/Page.tsx
@@ -1,50 +1,10 @@
 import { ReactNode } from "react";
 
-import { Box, HStack, Separator, Show } from "@chakra-ui/react";
-
-import { DPA } from "@/features/dpa/comps/DPA";
-
-import { NavBar } from "../parts/NavBar";
-
 export const Page = ({
   children,
-  sidebar,
 }: {
   children: ReactNode;
   sidebar?: ReactNode;
 }) => {
   return children;
-
-  return (
-    <HStack maxW="90rem" w="full" mx="auto" px={6} pt="6" gap={16} align="top">
-      <Box
-        // Quick fix, allows to size overflowing UserTree and polls correctly.
-        maxW="80%"
-        minH="90vh"
-        flexGrow={1}
-        position="relative"
-        pt={9}
-        pb={20}
-      >
-        {children}
-      </Box>
-      {/* Box makes sticky work */}
-      <Box>
-        <Box
-          w={{ lg: "12rem", xl: "14rem" }}
-          flexShrink={0}
-          mt={20}
-          position="sticky"
-          top={20}
-        >
-          <NavBar fontSize={{ lg: "xl", xl: "2xl" }} />
-          <Show when={sidebar}>
-            <Separator borderWidth={1} my={8} />
-            {sidebar}
-          </Show>
-        </Box>
-      </Box>
-      <DPA />
-    </HStack>
-  );
 };
